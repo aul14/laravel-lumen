df1911ccf91ae4aec424155031fca14dc913c5e3
aul14 <rahmanaulia336@gmail.com>
2022-10-07 14:50:57 +0700

    first commit

---
+++ database/migrations/2022_10_07_065216_create_notes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title');
+            $table->text('description');
+            $table->double('count');
+            $table->enum('type', ['expense', 'income']);
+            $table->double('price');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+};
